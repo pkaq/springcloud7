8e2081962802392750116ecc0c7096f3d65d4772
pkaq <pkaq@msn.com>
2017-09-06 08:33:54 +0000

    update class name

--- cloud-eureka-server/src/main/kotlin/org/pkaq/EurekaBooter.kt
+++ cloud-eureka-server/src/main/kotlin/org/pkaq/EurekaBooter.kt
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 // 激活为eureka\服务器
 @EnableEurekaServer
 @SpringBootApplication
-open class Booter: CommandLineRunner{
+open class EurekaBooter: CommandLineRunner{
     @Throws(Exception::class)
     override fun run(vararg args: String) {
         println("  --- --- --- [ eureka server started ] --- --- ---  ")
@@ -15,5 +15,5 @@ open class Booter: CommandLineRunner{
 }
 
 fun main(args: Array<String>) {
-    org.springframework.boot.SpringApplication.run(org.pkaq.Booter::class.java, *args)
+    org.springframework.boot.SpringApplication.run(org.pkaq.EurekaBooter::class.java, *args)
 }
