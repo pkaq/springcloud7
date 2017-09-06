c47a895d4b072b3482f88a2d470f783458289a24
pkaq <pkaq@msn.com>
2017-09-06 08:29:29 +0000

    add request mapping

--- cloud-docker-simple/src/main/kotlin/org/pkaq/DockerBooter.kt
+++ cloud-docker-simple/src/main/kotlin/org/pkaq/DockerBooter.kt
@@ -2,8 +2,11 @@ package org.pkaq
 
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 open class DockerBooter : CommandLineRunner {
 
     @Throws(Exception::class)
@@ -16,5 +19,10 @@ open class DockerBooter : CommandLineRunner {
         fun main(args: Array<String>) {
             org.springframework.boot.SpringApplication.run(org.pkaq.DockerBooter::class.java, *args)
         }
+    }
+
+    @RequestMapping("/")
+    fun index(): String {
+        return "Hello Spring cloud  Docker "
     }
 }
