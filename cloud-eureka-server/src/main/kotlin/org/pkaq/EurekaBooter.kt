1accb78d0474c35aa64d54f3bf428a82af007ed5
pkaq <pkaq@msn.com>
2017-09-06 21:10:11 +0000

    eureka demo codeOC

--- cloud-eureka-server/src/main/kotlin/org/pkaq/EurekaBooter.kt
+++ cloud-eureka-server/src/main/kotlin/org/pkaq/EurekaBooter.kt
@@ -8,12 +8,18 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 @EnableEurekaServer
 @SpringBootApplication
 open class EurekaBooter: CommandLineRunner{
+
     @Throws(Exception::class)
     override fun run(vararg args: String) {
         println("  --- --- --- [ eureka server started ] --- --- ---  ")
     }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            org.springframework.boot.SpringApplication.run(org.pkaq.EurekaBooter::class.java, *args)
+        }
+    }
+
 }
 
-fun main(args: Array<String>) {
-    org.springframework.boot.SpringApplication.run(org.pkaq.EurekaBooter::class.java, *args)
-}
